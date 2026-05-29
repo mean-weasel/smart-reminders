4c58e626fc180a0df7f1dff2b5806118e7ad3511
neonwatty <jermwatt@gmail.com>
2026-05-29 13:26:36 -0700

    feat: add cloud parser adapter

--- SmartReminders/Parsing/ParserClient.swift
+++ SmartReminders/Parsing/ParserClient.swift
@@ -28,3 +28,28 @@ struct ParsedDraftGroup: Codable, Equatable {
     var title: String
     var mode: String
 }
+
+struct CloudParserClient: ParserClient {
+    var endpoint: URL
+    var apiKey: String
+    var urlSession: URLSession = .shared
+
+    func parse(text: String) async throws -> ParserResponse {
+        var request = URLRequest(url: endpoint)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.httpBody = try JSONEncoder().encode(["text": text])
+
+        let (data, response) = try await urlSession.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+            throw ParserClientError.requestFailed
+        }
+
+        return try JSONDecoder().decode(ParserResponse.self, from: data)
+    }
+}
+
+enum ParserClientError: Error {
+    case requestFailed
+}
